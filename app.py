bff7faed57f04e267159b32834cdd944fee96043
Bán Marcell <afga93+cc@gmail.com>
2017-04-19 10:16:38 +0200

    hello world

---
+++ app.py
@@ -0,0 +1,14 @@
+"""
+KraKen the Code webapp
+"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+def root():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
