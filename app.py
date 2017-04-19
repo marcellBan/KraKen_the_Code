a9a54206dcb3b0a11931c9e84e626ac2f1c33c1d
GreywindGit <GreywindGit@users.noreply.github.com>
2017-04-19 12:14:16 +0200

    basic bootstrap layout

--- app.py
+++ app.py
@@ -10,5 +10,16 @@ app = Flask(__name__)
 def root():
     return "Hello World!"
 
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello(name=None):
+    return render_template('hello.html', name=name.capitalize() if name else name)
+
+
+@app.route("/jazmin")
+def jazmin():
+    return "Good morning"
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
