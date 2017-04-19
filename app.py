03d30a570faa745fd76d4775f604d844d85b06b8
Bán Marcell <afga93+cc@gmail.com>
2017-04-19 15:29:01 +0200

    got login working

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ KraKen the Code webapp
 """
 
 import json
-from flask import Flask, render_template, make_response, url_for
+from flask import Flask, request, session, redirect, render_template, make_response, url_for
 app = Flask(__name__)
 
 
 @app.route("/")
 def root():
-    return render_template("index.html", main_js=url_for('static', filename='js/main.js'))
+    return render_template("index.html",
+                           main_js=url_for('static', filename='js/main.js'),
+                           tools_js=url_for('static', filename='js/tools.js'))
 
 
 # @app.route("/hi")
@@ -24,9 +26,26 @@ def hello(name=None):
     return render_template('hello.html', name=name.capitalize() if name else name)
 
 
-@app.route("/jazmin")
-def jazmin():
-    return "Good morning"
+@app.route("/modules")
+def module_chooser():
+    if resuest.args.get("usr"):
+        print("login: " + request.args.get("usr"))
+        session['usr'] = request.args.get("usr")
+
+    return render_template('modules.html',
+                           main_js=url_for('static', filename='js/main.js'),
+                           tools_js=url_for('static', filename='js/tools.js'),
+                           logout=url_for('logout'))
+
+
+@app.route("/logout")
+def logout():
+    print("logout: " + session['usr'])
+    session.pop('usr', None)
+    return redirect(url_for('root'))
+
 
 if __name__ == "__main__":
+    # TODO: remove this
+    app.secret_key = 'test'
     app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,6 @@
 var onSignIn = function (googleUser) {
     var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
-
-var signInCallback = function (authResult) {
-    //debugger;
-    // ajax back to server
-    if(authResult["code"]){
-        console.log('code: ' + authResult["code"])
-    }
+    window.location.replace('/modules?usr=' + MD5(profile.getEmail()))
 }
 
 var onFailure = function (error) {
@@ -20,7 +9,7 @@ var onFailure = function (error) {
 
 var signOut = function () {
     auth2.signOut().then(function () {
-        console.log('User signed out.');
+        window.location.replace('/logout')
     });
 }
 
